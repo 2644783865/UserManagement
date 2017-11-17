69f5285f486beb1325bbfc6c1d03c45b1374095c
Team Till <TeamTill@users.noreply.github.com>
2017-11-17 11:44:45 +0000

    WinForms Consumer App added

--- UserManagement/Controllers/HomeController.cs
+++ UserManagement/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace UserManagement.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Put something else here";
 
             return View();
         }
